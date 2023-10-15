94f84f87cd814f6f0c2432b7d64ffbb4607c7065
MrBr00d <nvliet@gmail.com>
2023-10-15 03:02:43 +0200

    Add: Add game window

--- game_window.py
+++ game_window.py
@@ -12,108 +12,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 import shutil
 import pickle
+from Main import Organizer, Game
 
-class Game:
-    def __init__(self, name, save_location):
-        """Game class containing all game and savefile information.
-        name: (str) name of the game.
-        save_location: (str) path of the savefile location"""
-        self.name = name
-        self.save_location = save_location
-        self.directory = '\\'.join(self.save_location.split("\\")[:-1])
-        self.profiles = [item.name for item in os.scandir(self.directory) if item.is_dir()]
-        self.save_extention = "." + self.save_location.split("\\")[-1].split(".")[-1]
-
-    def get_name(self):
-        return self.name
-    
-    def get_location(self):
-        return self.directory
-    
-    def get_profiles(self):
-        return self.profiles
-    
-    def set_location(self, save_location):
-        self.save_location = save_location
-        self.directory = '\\'.join(self.save_location.split("\\")[:-1])
-        Organizer.save_dict()
-    
-    #Profile management functions 
-    def create_profile(self, profile_name):
-        os.mkdir(os.path.join(self.directory, profile_name))
-        print("Profile added.")
-
-    def remove_profile(self, profile_name):
-        shutil.rmtree(os.path.join(self.directory, profile_name))
-
-    def show_profile(self):
-        return [item.name for item in os.scandir(self.directory) if item.is_dir()]
-    
-    def get_sections(self, profile_name):
-        return[item.name for item in os.scandir(os.path.join(self.directory, profile_name)) if item.is_dir()]
-    
-    def import_save(self, profile, section):
-        """Imports the save to a profile and section. Profile can be any% for instance and section the part of the run to be saved.
-        profile: (str) e.g. any%/all bosses.
-        section: (str) e.g. intro/first bossfight/final boss"""
-        save_target = os.path.join(self.directory, profile, section, self.save_location.split("\\")[-1])
-        save_dir = os.path.join(self.directory, profile, section)
-        if os.path.isdir(f"{self.directory}\\{profile}"):
-            os.makedirs(save_dir, exist_ok=True)
-            shutil.copyfile(self.save_location, save_target)
-        else:
-            print("Profile not found.")
-
-    def load_save(self, profile, section):
-        save_target = os.path.join(self.directory, profile, section, self.save_location.split("\\")[-1])
-        shutil.copy(save_target, self.directory)
-
-class Organizer:
-    Game_dict: dict[str, Game] = {}
-    def __init__(self):
-        """Upon starting the program the games are loaded if the file exists"""
-        if "Organizer.Game_dict.pkl" in os.listdir():
-            Organizer.load_dict()
-            print("Organizer.Game_dict loaded.")
-    
-    @staticmethod
-    def add_game(name, save_location):
-        """Function to add a game and create a game instance. This instance is then saved in the Organizer.Game_dict dictionary and saved.
-        name: (str) name of the game.
-        save_location: (str) location of the savefile."""
-        if name not in Organizer.Game_dict.keys():
-            Organizer.Game_dict[name] = Game(name, save_location)
-            Organizer.save_dict()
-            print("Game added.")
-        else:
-            print("Game already in game list.")
-    @staticmethod
-    def remove_game(name):
-        """Remove game from dictionary and saves to file.
-        name: (str) name of the game to be removed"""
-        if name in Organizer.Game_dict.keys():
-            Organizer.Game_dict.pop(name)
-            Organizer.save_dict()
-            print("Game removed.")
-        else:
-            print("Game not found.")
-
-    @staticmethod
-    def get_games():
-        """Lists all games in the Games_dict file"""
-        return list(Organizer.Game_dict.keys())
-    
-    @staticmethod
-    def load_dict():
-        """Function that loads in previously added games"""
-        with open('Organizer.Game_dict.pkl', 'rb') as f:
-            Organizer.Game_dict = pickle.load(f)
-    
-    @staticmethod
-    def save_dict():
-        """Function that serializes all games"""
-        with open("Organizer.Game_dict.pkl", 'wb') as f:
-            pickle.dump(Organizer.Game_dict, f)
 
 m = Organizer()
 
@@ -125,8 +25,10 @@ class Ui_MainWindow(object):
             self.list_game.addItem(item)
     
     def add_ds3(self):
-        m.add_game("DS3", str(QtWidgets.QFileDialog.getOpenFileName()[0].replace("/", "\\")))
-        self.populate_list()
+        # m.add_game("DS3", str(QtWidgets.QFileDialog.getOpenFileName()[0].replace("/", "\\")))
+        a = str(QtWidgets.QFileDialog.getOpenFileName()[0])
+        print(a)
+        # self.populate_list()
 
     def remove_game(self):
         todel = self.list_game.currentItem().text()
@@ -146,6 +48,7 @@ class Ui_MainWindow(object):
         self.button_add = QtWidgets.QPushButton(self.centralwidget)
         self.button_add.setGeometry(QtCore.QRect(380, 40, 111, 23))
         self.button_add.setObjectName("button_add")
+        self.button_add.clicked.connect(self.add_ds3)
         self.button_remove = QtWidgets.QPushButton(self.centralwidget)
         self.button_remove.setGeometry(QtCore.QRect(380, 70, 111, 23))
         self.button_remove.setObjectName("button_remove")
@@ -170,11 +73,13 @@ class Ui_MainWindow(object):
         self.button_remove.setText(_translate("MainWindow", "Remove"))
         self.populate_list()
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_MainWindow()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
+#     sys.exit(app.exec_())
+
+print(m.Game_dict)
